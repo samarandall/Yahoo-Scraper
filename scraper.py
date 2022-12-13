a3dde079af3816a4a98a7dbad5daf7b55cfa0a97
Sam <samrandall100@gmail.com>
2022-12-13 16:59:36 -0600

    completed the parser for qb

--- scraper.py
+++ scraper.py
@@ -4,49 +4,39 @@ from dataclasses import dataclass, asdict
 
 league_number = 528 # change for different league
 
-url = f'https://football.fantasysports.yahoo.com/f1/{league_number}/players?&sort=AR&sdir=1&status=ALL' # will need to change since data is based on my league
+base_url = f'https://football.fantasysports.yahoo.com/f1/{league_number}/players?&sort=AR&sdir=1&status=ALL' # will need to change since data is based on my league
 
-base_url = 'https://football.fantasysports.yahoo.com/f1/145337/players'
+#base_url = 'https://football.fantasysports.yahoo.com/f1/145337/players'
 
 @dataclass
 class Player:
     player: str
     position: str
     projected_pts: str
-    
-def get_qb(base_url):
-    full_url = f'{base_url}&pos=QB&stat1=S_PS_2022&jsenabled=1' # projected season stats
+
+def get_pos(pos):
+    full_url = f'{base_url}&pos={pos}&stat1=S_PS_2022&jsenabled=1'
     resp = httpx.get(full_url)
     html = HTMLParser(resp.text)
-    #return HTMLParser(resp = httpx.get(f'{base_url}&pos=QB&stat1=S_PS_2022&jsenabled=1'))
     return html
-    
-def get_wr(base_url):
-    full_url = f'{base_url}&pos=WR&stat1=S_PS_2022&jsenabled=1'
-    return
 
-def get_rb(base_url):
-    full_url = f'{base_url}&pos=RB&stat1=S_PS_2022&jsenabled=1'
-    return
-
-def get_te(base_url):
-    full_url = f'{base_url}&pos=TE&stat1=S_PS_2022&jsenabled=1'
-    return
-
-def parse(html):
-    player_table = html.css('div.ysf-player-name')
-    player_table = html.css('a.Nowrap')
+def parse(html,pos):
+    player_name = html.css('div.ysf-player-name')
+    player_projected_pts= html.css('span.Fw-b')
+    player_table = zip(player_name,player_projected_pts)
+    #print(player_projected_pts, player_name)
     for player in player_table:
         new_player = Player(
-            player=player.css_first().text(),
-            position="QB",
-            projected_pts='1'
+            player=player[0].text(),
+            position=pos,
+            projected_pts=player[1].text()
         )
         print(new_player)
 
 def main():
-    qb_html = get_qb(base_url=url)
-    parse(qb_html)
+    #qb_html = get_qb(base_url=url)
+    qb_html = get_pos(pos='QB')
+    parse(html=qb_html, pos='QB')
     #print(qb_html.css_first('title').text())
 
 if __name__ == '__main__':
